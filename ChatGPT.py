bf9d6f833acde0e1a16d28cfa7cfd1fc47b1e058
ssoxong <ssoxong@naver.com>
2023-08-18 02:32:01 +0900

    test: before merge feature_02

--- ChatGPT.py
+++ ChatGPT.py
@@ -21,5 +21,3 @@ def ChatGPT(input_msg):
 
 # 응답에서 생성된 텍스트 출력
     return (response.choices[0].message['content'].strip())
-
-# ChatGPT("난 너를 사랑해 i love you girl 세상은 너 뿐이야. 소리쳐 부르지만 저 대답없는 노을만 붉게 타는데.")
