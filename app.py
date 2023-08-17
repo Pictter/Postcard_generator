40fb5d4641461c509bb06b355be0357f5f71ad62
ssoxong <ssoxong@naver.com>
2023-08-18 01:18:30 +0900

    feat: First Merge

--- app.py
+++ app.py
@@ -3,6 +3,7 @@ from ChatGPT import ChatGPT
 from DALLE2 import dalle
 
 app = Flask(__name__)
+keyword=""
 
 @app.route('/')
 def root():
@@ -13,12 +14,20 @@ def index():
     return render_template("main.html")
 
 @app.route('/letter', methods=['POST'])
-def result():
+def letter():
     letter = request.form.get('letter')
+    global keyword 
     keyword = ChatGPT(letter)
-    image = dalle(keyword)
-    print(image)
+    # image = dalle(keyword)
+    # print(image)
     return render_template("letter.html",keyword=keyword)
+
+@app.route('/image')
+def img():
+    global keyword
+    
+
+
 
 if __name__ == '__main__':
     app.run(debug=True)
