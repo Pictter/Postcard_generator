6d39f288b488bc5751865a823984c480cee82460
hellokms8828 <hellokms8828@naver.com>
2023-11-09 14:36:33 +0900

    연동

--- ChatGPT.py
+++ ChatGPT.py
@@ -7,17 +7,45 @@ dotenv_file = dotenv.find_dotenv()
 dotenv.load_dotenv(dotenv_file)
 openai.api_key = os.environ["API_KEY"]
 
+
 # ChatGPT API 호출
 def ChatGPT(input_msg):
     response = openai.ChatCompletion.create(
-        model="gpt-3.5-turbo",  # 사용할 엔진 선택 (gpt
+        model="gpt-3.5-turbo",  # 사용할 엔진 선택
         messages=[
-            {"role": "system", "content": "Look at the mood of this message, select 5 keywords, and return the results in English."},
-            {"role": "system", "content": "in the form of word1, word2, word3, word4, word5."},
-            {"role": "user", "content": input_msg}
+            {
+                "role": "system",
+                "content": "이 메시지의 분위기를 파악하여 5개의 키워드를 선택하고, 결과를 한국어로 반환하십시오.",
+            },
+            {
+                "role": "system",
+                "content": "단어1, 단어2, 단어3, 단어4, 단어5 형태로 반환하십시오.",
+            },
+            {"role": "user", "content": input_msg},
         ],
         max_tokens=50,  # 생성될 최대 토큰 수
     )
-    
-# 응답에서 생성된 텍스트 출력
-    return (response.choices[0].message['content'].strip())
+
+    # 응답에서 생성된 텍스트 출력
+    return response.choices[0].message["content"].strip()
+
+
+def ChatGPT2(input_msg):
+    response = openai.ChatCompletion.create(
+        model="gpt-3.5-turbo",  # 사용할 엔진 선택 (gpt
+        messages=[
+            {
+                "role": "system",
+                "content": "Convert Korean words to English.",
+            },
+            {
+                "role": "system",
+                "content": "in the form of word1, word2, word3, word4, word5.",
+            },
+            {"role": "user", "content": input_msg},
+        ],
+        max_tokens=50,  # 생성될 최대 토큰 수
+    )
+
+    # 응답에서 생성된 텍스트 출력
+    return response.choices[0].message["content"].strip()
--- app.py
+++ app.py
@@ -1,8 +1,9 @@
 from flask import Flask, request, render_template, session, send_file, render_template
-from ChatGPT import ChatGPT
+from ChatGPT import ChatGPT, ChatGPT2
 from DALLE2 import dalle
 import urllib.request
 from Etri import Etri
+from image_text import process_cloudsight
 
 from _auth import random_string, md5_hash
 
@@ -11,49 +12,66 @@ app = Flask(__name__)
 app.secret_key = md5_hash(random_string(32))
 
 
-
 @app.route("/")
 def index():
     return render_template("main.html")
 
-@app.route('/letter', methods=['POST'])
+
+@app.route("/letter", methods=["POST"])
 def letter():
     letter_text = request.form.get("letter")
-    print("letter_text",letter_text)
-    #keyword = Etri(letter_text).split(', ')
+    print("letter_text", letter_text)
     keyword = Etri(letter_text)
+    ##keyset = "".join(Etrikey)
+    # keyword = ChatGPT(letter_text + keyset).split(", ")
+    print("keyword", keyword)
+
     session["input_text_"] = letter_text
     session["keyword_"] = keyword  # 세션에 값을 저장
     return render_template("letter.html", keyword=keyword)
 
 
-@app.route('/image', methods=['GET'])
+@app.route("/image", methods=["GET"])
 def img():
     keyword = session.get("keyword_")
     input_text_ = session.get("input_text_")
-    print(keyword)
-    print("input_text", input_text_)
-    image_response = dalle(keyword)
+    # print("input_text", input_text_)
+    newkeyworld = ChatGPT2("".join(keyword)).split(", ")
+
+    image_response = dalle(newkeyworld)
     image_url = []
     for i in range(0, 3, 1):
         image_url.append(image_response["data"][i]["url"])
-    
-    print("image_url",image_url)
+
+    print("image_url", image_url)
     return render_template("image.html", image_url=image_url, letter=input_text_)
 
-@app.route('/selected-image', methods=['POST'])
+
+@app.route("/selected-image", methods=["POST"])
 def selected_image():
     data = request.json
-    img_src = data['imgSrc']
-    session["image_url_"]=img_src
-    
-    return render_template("selected-image.html",image_url=img_src, letter = session["input_text_"] )
+    img_src = data["imgSrc"]
+    session["image_url_"] = img_src
+    textres = process_cloudsight(img_src)
+    text = textres["caption"]
+    print(text)
 
-@app.route('/download')
-def download():
-    url = request.args.get('image_url')  
-    # url = "https://oaidalleapiprodscus.blob.core.windows.net/private/org-udHcUVpfVyx2Nt3o462FR5QA/user-m6jgVNuFOIGosEsQ3NG9sPgk/img-8Be3GGC778CWmd2Ef4fiKO8D.png?st=2023-08-17T19%3A57%3A28Z&se=2023-08-17T21%3A57%3A28Z&sp=r&sv=2021-08-06&sr=b&rscd=inline&rsct=image/png&skoid=6aaadede-4fb3-4698-a8f6-684d7786b067&sktid=a48cca56-e6da-484e-a814-9c849652bcb3&skt=2023-08-17T17%3A53%3A08Z&ske=2023-08-18T17%3A53%3A08Z&sks=b&skv=2021-08-06&sig=5DvvsgJSRVVAM4CwlQB2ccs4OkgaGzoSqkzLDwEXMwA%3D"
-    urllib.request.urlretrieve(url, "test.jpg")
+    return render_template(
+        "selected-image.html",
+        image_url=img_src,
+        letter=session["input_text_"],
+        text=text,
+    )
+
+
+@app.route("/test", methods=["GET"])
+def test_html():
+    return render_template(
+        "test.html",
+        image_url="https://i.namu.wiki/i/SLLmySVvTaIb51ygri4wwte51MZEOcfmdQUnMz3TnNPdYEuQM80Wgn3pONY1mBon0GIaZyvmowwqm8JEHGHDkw.webp",
+        letter="아무거나 써볼게요",
+        text="설명입니다",
+    )
 
 
 if __name__ == "__main__":
