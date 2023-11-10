1c60fca993fd054d8451dcfb077bfa5ee03e3f9b
naindae <98092591+naindae@users.noreply.github.com>
2023-11-10 16:27:10 +0900

    Delete translate.py

--- translate.py
+++
@@ -1,7 +0,0 @@
-from google.cloud import translate_v2 as translate
-
-# Google Cloud Translation API를 설정합니다.
-def translate_text(text, target_language='ko'):
-    translate_client = translate.Client()
-    result = translate_client.translate(text, target_language=target_language)
-    return result['translatedText']
